bf5a411df733d4cd4871d81487f36fe986a7e5e2
Edmund Li <edmund.xz.lee@gmail.com>
2016-08-04 23:10:55 -0700

    test page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  get :test, to: 'static_pages#test'
+
   resources :alarms, only: [:create, :update, :destroy] do
     member do
       post :ring
