478561886fca22e3cc79b490dede1b8199b1cec1
Edmund Li <edmund.xz.lee@gmail.com>
2016-10-02 17:08:38 -0700

    load lib dir

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Socialarm
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.autoload_paths << Rails.root.join('lib/integrations')
   end
 end
